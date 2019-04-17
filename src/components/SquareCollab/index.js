91f01ef46304b20c53def2e654d31127cf7969de
LORDBABUINO <jorge.x7@gmail.com>
2019-04-17 19:57:55 +0000

    Adds styles to square-collab

--- src/components/SquareCollab/index.js
+++ src/components/SquareCollab/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './styles.css'
 
 const SquareCollab = () => (
-	<button>X</button>
+	<button className="square-collab">X</button>
 )
 
 export default SquareCollab
