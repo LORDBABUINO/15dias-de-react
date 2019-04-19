92a2210e7887eb9cf214b3cd66baf57fbe27a4cd
LORDBABUINO <jorge.x7@gmail.com>
2019-04-19 00:27:31 +0000

    Completes the basic challenges from Day 03

--- src/components/BoardCollab/index.js
+++ src/components/BoardCollab/index.js
@@ -3,8 +3,18 @@ import SquareCollab from '../SquareCollab';
 
 import './styles.css'
 
+const squareCollabs = qtd => {
+	const squares = []
+
+	while(qtd--){
+		squares.push(<SquareCollab key={qtd}/>)
+	}
+	
+	return squares
+}
+
 const BoardCollab = ({qtd}) => (
-	<article className="board-collab"><SquareCollab /></article>
+	<article className="board-collab">{squareCollabs(qtd)}</article>
 )
 
 export default BoardCollab;
